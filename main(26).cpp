2479a78c9a2e5e4e4f41343a350978b55eda1055
danielnlz01 <38385574+danielnlz01@users.noreply.github.com>
2021-08-31 21:04:22 -0500

    Add files via upload

---
+++ main(26).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void bubbleSort(int a[], int len);
+
+int main() 
+{
+	int d[10] = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+	
+// i=0, i+1
+//checar si la pos 0 es mayor a la que sigue, si es, cambiar pos, si no es, se deja igual. luego hace lo mismo para la siguiente pos
+  return 0;
+}
+
+void bubbleSort(int a[], int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		
+	}
+}
